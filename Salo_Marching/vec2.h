eb22797ed92bb6cc8be1be68d12e32a600a288c2
SaloEdusBEER <166428756+SaloEdusBEER@users.noreply.github.com>
2025-06-26 19:23:02 +0300

    добавил пару функций для читабельности

---
+++ Salo_Marching/vec2.h
@@ -0,0 +1,26 @@
+#pragma once
+struct vec2
+{
+public:
+	float x, y;
+
+	vec2() : x(0), y(0) {}
+	vec2(float value) : x(value), y(value) {}
+	vec2(float x, float y) : x(x), y(y) {}
+
+	vec2 operator+(const vec2& other) const {
+		return vec2(x + other.x, y + other.y);
+	}
+
+	vec2 operator-(const vec2& other) const {
+		return vec2(x - other.x, y - other.y);
+	}
+
+	vec2 operator*(const vec2& other) const {
+		return vec2(x * other.x, y * other.y);
+	}
+
+	vec2 operator/(const vec2& other) const {
+		return vec2(x / other.x, y / other.y);
+	}	
+};
